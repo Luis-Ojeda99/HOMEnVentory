58d675697812482403eb09fb27878f152ee6b5df
Luis <luisojeda27180@gmail.com>
2020-08-09 21:18:41 -0400

    Account management implemented

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('register/', views.register, name='register'),
+    path('', views.index_view, name='index'),
+    path('about/', views.about_view, name='about'),
+    path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('user-inventory/', views.user_inventory, name='user-inventory')
+    path('user-inventory/', views.user_inventory, name='user-inventory'),
+    path('account-management/', views.account_management_view, name="account-management"),
+    path('new-password/', views.new_password_view, name='new-password'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
-from .forms import RegistrationForm, AccountAuthenticationForm
+from .forms import RegistrationForm, AccountAuthenticationForm, AccountUpdateForm
+from .models import Item
 
 
 # Create your views here.
-def index(request):
+def index_view(request):
     return render(request, 'index.html')
 
 
@@ -39,15 +40,23 @@ def login_view(request):
     return render(request, "registration/login.html", context)
 
 
-def about(request):
+def about_view(request):
     return render(request, 'about.html')
 
 
 def user_inventory(request):
-    return render(request, 'user_inventory.html')
+
+    if not request.user.is_authenticated:
+        return redirect("login")
+
+    context = {}
+
+    items = Item.objects.filter(owner=request.user)
+    context['items'] = items
+    return render(request, 'user_inventory.html', context)
 
 
-def register(request):
+def register_view(request):
     context = {}
     if request.POST:
         form = RegistrationForm(request.POST)
@@ -65,3 +74,36 @@ def register(request):
         context['registration_form'] = form
 
     return render(request, 'registration/register.html', context)
+
+
+def account_management_view(request):
+
+    if not request.user.is_authenticated:
+        return redirect("login")
+
+    context = {}
+
+    if request.POST:
+        form = AccountUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            context['Success_message'] = 'Your account has been updated.'
+    else:  # GET request.
+        form = AccountUpdateForm(
+            initial={
+                "username": request.user.username,
+                "email": request.user.email,
+                "first_name": request.user.first_name,
+                "last_name": request.user.last_name
+            }
+        )
+
+    context['account_form'] = form
+
+    return render(request, "account_management.html", context)
+
+
+def new_password_view(request):
+
+    context = {}
+    return render(request, "new_password.html", context)
