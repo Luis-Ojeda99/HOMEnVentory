706508d2062f24f67ed3ceaf10f50d7eae32e7b2
Luis Ojeda <luisojeda27180@gmail.com>
2025-09-02 09:22:07 -0600

    fix: login and logout view properly show up

--- HOMEnVentory/urls.py
+++ HOMEnVentory/urls.py
@@ -21,6 +21,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('', include('inventory.urls')),
-    path('auth/', include('django.contrib.auth.urls')),
+    # path('auth/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
